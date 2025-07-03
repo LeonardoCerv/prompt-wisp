d237a3efde8cd2a5ccae20087511b62ef6fccaf2
onyxdude <leocerva29@gmail.com>
2025-07-03 16:10:02 -0600

    [feat] update to the privacy policy

--- src/app/(routes)/privacy-policy/page.tsx
+++ src/app/(routes)/privacy-policy/page.tsx
@@ -51,6 +51,68 @@ export default async function PrivacyPolicyPage() {
             This policy may change as the app evolves. Continued use of the app
             means you accept the latest version.
           </p>
+          <h2 className="text-2xl font-medium text-moonlight-silver-bright mt-8 mb-2">Privacy Policy for the Chrome Extension</h2>
+          <p>
+            <strong>Privacy Policy</strong>
+          </p>
+          <p>
+            <strong>Introduction</strong>
+          </p>
+          <p>
+            This privacy policy describes how our extension handles user data. We are committed to protecting our users' privacy and ensuring their data is treated securely and responsibly.
+          </p>
+          <p>
+            <strong>Data Collection</strong>
+          </p>
+          <p>
+            Our extension does not collect personal or sensitive user information. This includes, but is not limited to:
+          </p>
+          <ul>
+            <li>Personally identifiable information (name, address, email, etc.).</li>
+            <li>Health information.</li>
+            <li>Financial and payment information.</li>
+            <li>Authentication information (passwords, PINs, etc.).</li>
+            <li>Personal communications (messages, emails, etc.).</li>
+            <li>Location (IP address, GPS coordinates, etc.).</li>
+            <li>Web history.</li>
+            <li>User activity (clicks, scrolls, etc.).</li>
+            <li>Website content (texts, images, videos, etc.).</li>
+          </ul>
+          <p>
+            <strong>Data Usage</strong>
+          </p>
+          <p>
+            If our extension interacts with user data, it will be exclusively to fulfill the specific functionalities of the extension. Data will not be sold, transferred, or used for purposes unrelated to the extension's objective.
+          </p>
+          <p>
+            <strong>Data Transfer</strong>
+          </p>
+          <p>
+            We certify that:
+          </p>
+          <ul>
+            <li>We do not sell or transfer user data to third parties, except in approved use cases.</li>
+            <li>We do not use or transfer user data for purposes unrelated to the specific purpose of the extension.</li>
+            <li>We do not use or transfer user data to determine creditworthiness or perform credit activities.</li>
+          </ul>
+          <p>
+            <strong>Security</strong>
+          </p>
+          <p>
+            We are committed to implementing appropriate security measures to protect any data that may be processed by the extension.
+          </p>
+          <p>
+            <strong>Changes to the Privacy Policy</strong>
+          </p>
+          <p>
+            We reserve the right to update this privacy policy at any time. Users will be notified of any significant changes.
+          </p>
+          <p>
+            <strong>Contact</strong>
+          </p>
+          <p>
+            If you have questions or concerns about this privacy policy, you can contact us through the channels provided in the extension's description.
+          </p>
         </div>
       </section>
       <Footer />
